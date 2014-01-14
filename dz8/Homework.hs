077ca0abebe1dbba97e7fd7aa66dd1ca81cd0246
msantl <msantl.ck@gmail.com>
2014-01-14 15:48:24 +0100

    Partial 8 homework

--- dz8/Homework.hs
+++ dz8/Homework.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE OverlappingInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
-import Prelude hiding (foldr,foldl,foldr1, all, elem, zipWith, sum)
+import Prelude hiding (foldr,foldl,foldr1, all, elem, zipWith, sum, maximum)
+import qualified Data.List as L
+import qualified Data.Char as C
 import Data.Functor
 import Data.Foldable hiding (toList, fromList)
 import Data.Maybe
@@ -168,7 +170,7 @@ class Vector v where
     nonzeros x  = filter (/=0) (toList x)
 
 -- 3.b
-data ListVector = LV [Double]
+data ListVector = LV [Double] deriving Show
 
 instance Vector ListVector where
     empty = LV []
@@ -192,18 +194,24 @@ instance Vector ListVector where
     toAssocList v = filter (\x -> snd x /= 0) $ zip [0..] (toList v)
 
 -- 3.c
-data SparseVector = SV (M.Map Int Double)
+data SparseVector = SV (M.Map Int Double) deriving Show
 
 instance Vector SparseVector where
     empty = SV (M.empty)
 
-    zipWith = undefined
+    zipWith f (SV x) (SV y) = fromAssocList lista
+        where
+            kljucevi = (M.keys x) ++ (M.keys y)
+            lista = [ (i, f (M.findWithDefault 0.0 i x) (M.findWithDefault 0.0 i y) ) | i <- kljucevi ]
 
-    fromList = undefined
-    toList   = undefined
+    fromList xs = SV (M.fromList [ (i, x) | (i, x) <- zip [0..] xs, x /= 0 ])
 
-    fromAssocList = undefined
-    toAssocList = undefined
+    toList (SV xs) = [M.findWithDefault 0 i xs | i <- [1..x] ]
+        where x = if null $ M.keys xs then 0 else maximum $ M.keys xs
+
+    fromAssocList xs = SV (M.fromList xs)
+
+    toAssocList (SV xs) = M.toList xs
 
 -- 3.d
 norm :: Vector v => v -> Double
@@ -217,4 +225,165 @@ cosine v1 v2
     where
         n1 = norm v1
         n2 = norm v2
+
+-- 4
+class Vectorizable a where
+    vectorize :: a -> SparseVector
+
+-- 4.a
+counts :: Ord a => [a] -> [(a, Int)]
+counts xs = [(head x, length x) | x <- (L.group $ L.sort xs)]
+
+-- 4.b
+instance Vectorizable [Char] where
+    vectorize xs = fromAssocList $ translate $ counts xs
+        where
+            translate []        = []
+            translate (y:ys)    = (C.ord $ fst y, fromIntegral $ snd y) : (translate ys)
+
+-- 4.c
+instance Vectorizable (Tree1 Int) where
+    vectorize bt = fromAssocList [ (x, fromIntegral y) | (x, y) <- zip [0..] (preorder bt)]
+
+-- 4.d
+t3 = Node1 5 (Leaf1 1) (Leaf1 5) :: Tree1 Int
+t4 = Leaf1 5 :: Tree1 Int
+
+search :: Vectorizable a => a -> [a] -> [a]
+search x xs = map snd $ L.sortBy cmp [(cosine (vectorize x) (vectorize y), y) | y <- xs]
+    where
+        cmp a b
+            | fst a > fst b = LT
+            | otherwise     = GT
+
+
+-- 5
+type Line = [Token]
+data Token = Word String | Blank | HypWord String deriving (Eq, Show)
+
+text = "He who controls the past controls the future. He who controls the present controls the past."
+
+-- 5.a
+string2line :: String -> Line
+string2line xs = [Word x | x <- words xs]
+
+-- 5.b
+line2string :: Line -> String
+line2string xs = L.unwords [x | Word x <- xs]
+
+-- 5.c
+tokenLength :: Token -> Int
+tokenLength (Word xs)       = length xs
+tokenLength (Blank)         = 1
+tokenLength (HypWord xs)    = 1 + length xs
+
+-- 5.d
+lineLength :: Line -> Int
+lineLength xs = sum [tokenLength x | x <- xs]
+
+-- 5.e
+breakLine :: Int -> Line -> (Line, Line)
+
+breakLine n xs = breakLine_ n xs []
+    where
+        breakLine_ n l@(x:xs) ys
+            | tokenLength x < n = breakLine_ (n - (tokenLength x)) xs (ys ++ [x])
+            | otherwise         = (ys, l)
+
+        breakLine_ _ [] ys = (ys, [])
+
+-- 5.f
+mergers :: [String] -> [(String, String)]
+mergers xs = [ concat' $ splitAt i xs | i <- [1..((length xs) - 1)] ]
+    where
+        concat' x = (L.concat $ fst x, L.concat $ snd x)
+
+-- 5.g
+type HypMap = M.Map String [String]
+
+enHyp :: HypMap
+enHyp = M.fromList [
+    ("controls", ["co", "nt", "ro" ,"ls"]),
+    ("future", ["fu", "tu", "re"]),
+    ("present", ["pre", "se", "nt"]) ]
+
+hyphenate :: HypMap -> Token -> [(Token, Token)]
+hyphenate mapa (Word xs) = map (\(x, y) -> (HypWord x, Word (append_dots broj_tocaka y))) $ mergers slogovi
+    where
+        slogovi = case (M.lookup (remove_dots xs) mapa) of
+            Just x -> x
+            Nothing -> []
+        tocaka ys
+            | null ys           = 0
+            | last ys == '.'    = 1 + (tocaka $ init ys)
+            | otherwise         = 0
+        remove_dots ys
+            | null ys           = []
+            | last ys == '.'    = remove_dots $ init ys
+            | otherwise         = ys
+        append_dots n ys
+            | n > 0     = append_dots (n-1) (ys ++ ".")
+            | otherwise = ys
+        broj_tocaka = tocaka xs
+
+-- 5.h
+lineBreaks :: HypMap -> Int -> Line -> [(Line,Line)]
+lineBreaks mapa n xs = [breakLine n y | y <- podjele]
+    where
+        podjele = [(init xs) ++ [fst y] ++ [snd y] | y <- (hyphenate mapa (last xs))]
+
+-- 5.i
+insertions :: a -> [a] -> [[a]]
+insertions needle haystack = [ (fst $ splitAt x haystack ) ++ [needle] ++ (snd $ splitAt x haystack) | x <- [0..(length haystack)]]
+
+-- 5.j
+insertBlanks :: Int -> Line -> [Line]
+insertBlanks = undefined
+
+-- 5.k
+blankDistances :: Line -> [Int]
+blankDistances = undefined
+
+-- 5.l
+avg :: Real a => [a] -> Double
+avg xs
+    | null xs   = 0
+    | otherwise = (realToFrac $ sum xs) / (realToFrac $ length xs)
+
+var xs = (var' 0 0 0 xs) / (realToFrac $ length xs - 1)
+    where
+      var' _ _ s [] = s
+      var' m n s (x:xs) = var' nm (n + 1) (s + delta * (x - nm)) xs
+         where
+           delta = x - m
+           nm = m + delta / (realToFrac $ n + 1)
+
+-- 5.m
+data Costs = Costs {
+    blankCost :: Double,
+    blankProxCost :: Double,
+    blankUnevenCost :: Double,
+    hypCost :: Double } deriving (Eq,Show)
+
+defaultCosts = Costs {
+    blankCost = 1,
+    blankProxCost = 1,
+    blankUnevenCost = 0.5,
+    hypCost = 0.5 }
+
+lineBadness :: Costs -> Line -> Double
+lineBadness = undefined
+
+-- 5.n
+bestLineBreak :: Costs -> HypMap -> Int -> Line -> Maybe (Line,Line)
+bestLineBreak = undefined
+
+-- 5.o
+justifyLine :: Costs -> HypMap -> Int -> Line -> [Line]
+justifyLine = undefined
+
+-- 5.p
+justifyString :: String -> [String]
+justifyString = undefined
+
 
